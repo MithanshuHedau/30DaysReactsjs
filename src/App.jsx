eec574b4a3c72b0ab5af55e1fa2863c5c99bdb03
mithanshu hedau <hedaumithanshu@gmail.com>
2024-09-01 15:15:48 +0530

    Day two

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="bg-gray-300 min-h-screen flex items-center justify-center">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
